6d2a97b5e5a7819a5dbdca580e552e1cf5eeab4f
yuribmontez <ybmontez@gmail.com>
2021-09-15 11:19:01 -0300

    feat: manage subscriptions with stripe webhook

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,57 @@
+import { query as q } from 'faunadb'
+import { fauna } from '../../../services/fauna'
+import { stripe } from '../../../services/stripe'
+
+export async function saveSubscription(subscriptionId: string, customerId: string, createAction = false) {
+  // Buscar usuario no banco do FaunaDB com o stripe-customer-id {customerId}
+  const userRef = await fauna.query(
+    q.Select(
+      'ref',
+      q.Get(
+        q.Match(
+          q.Index('user-by-stripe-customer-id'),
+          customerId
+        )
+      )
+    )
+  )
+
+  // Pegando dados da subscription
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+
+  // Dados que quero salvar
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id
+  }
+
+  // Se estiver criando nova subscription
+  // Salvar os dados da subscription no FaunaDB
+  if (createAction) {
+    await fauna.query(
+      q.Create(
+        q.Collection('subscriptions'),
+        { data: subscriptionData }
+      )
+    )
+  // Se não, atualizar dados da subscription
+  } else {
+    await fauna.query(
+      q.Replace(
+        q.Select(
+          'ref',
+          q.Get(
+            q.Match(
+              q.Index('subscription-by-id'),
+              subscriptionId,
+            )
+          )
+        ),
+        { data: subscriptionData }
+      )
+    )
+  }
+
+}
